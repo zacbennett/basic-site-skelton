21ea18fd1d5118222b68e15bc42a9ec36f83988d
Zac B <zacbennett0@gmail.com>
2020-11-28 10:12:50 -0800

    small changes

--- src/App.js
+++ src/App.js
@@ -11,16 +11,10 @@ function App() {
   return (
     <div id="app">
       <Router>
-        <div className="header">Header</div>
         <div className="navigation">
-          <ul>
-            <li>
+         
               <Link to="/">Home</Link>
-            </li>
-            <li>
               <Link to="/about">About</Link>
-            </li>
-          </ul>
         </div>
         <div className="main">
           <Switch>
